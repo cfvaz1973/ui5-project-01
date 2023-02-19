beb519425fab1fccfc6a93271f6163f43c54a57a
cfvaz1973 <cfvaz1973@gmail.com>
2023-02-19 18:57:04 +0000

    reverse

--- webapp/index.js
+++ webapp/index.js
@@ -2,7 +2,6 @@ sap.ui.define(
   ["sap/m/Button", "sap/ui/core/mvc/XMLView"],
   function (Button, XMLView) {
     "use strict";
-
     XMLView.create({
       viewName: "sap.ui.demo.view.App",
     }).then(function (oView) {
